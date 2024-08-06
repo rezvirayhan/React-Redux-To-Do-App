0f22eddc1702ae6601d9102048b000f1ad53ec15
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2024-08-06 13:54:25 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <Button>Hello Sadecdn Button</Button>
+    </div>
+  );
+};
+
+export default App;
